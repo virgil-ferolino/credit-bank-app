053aba6a208626b4b7fb46d02271d572242de579
cedy123 <cedrickgueta056@gmail.com>
2025-02-07 17:48:30 +0800

    revert code layout

--- app/(mycard)/_layout.tsx
+++ app/(mycard)/_layout.tsx
@@ -1,42 +1,23 @@
 import IosBackButton from "@/components/IosBackButton";
 import { Stack } from "expo-router";
-import { Platform } from "react-native";
-import styled from "styled-components/native";
 
 const MyCardsLayout = () => {
   return (
-    <Container>
-      <Content>
-        <Stack
-          screenOptions={{
-            headerRight: () => <IosBackButton />,
-          }}
-        >
-          <Stack.Screen
-            name="addNewCard"
-            options={{
-              title: "Add New Card",
-              headerBackVisible: true,
-              headerTitleAlign: "center",
-            }}
-          />
-        </Stack>
-      </Content>
-    </Container>
+    <Stack
+      screenOptions={{
+        headerRight: () => <IosBackButton />,
+      }}
+    >
+      <Stack.Screen
+        name="addNewCard"
+        options={{
+          title: "Add New Card",
+          headerBackVisible: true,
+          headerTitleAlign: "center",
+        }}
+      />
+    </Stack>
   );
 };
-
-const Container = styled.View`
-  flex: 1;
-  background-color: white;
-  align-items: ${Platform.OS === "web" ? "center" : "stretch"};
-  justify-content: flex-start;
-`;
-
-const Content = styled.View`
-  flex: 1;
-  width: ${Platform.OS === "web" ? "100%" : "100%"};
-  max-width: 480px;
-`;
 
 export default MyCardsLayout;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,15 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
 
 import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
+
 import { useTheme } from "react-native-paper";
 
 export default function TabLayout() {
   const theme = useTheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: theme.colors.primary,
-        headerShown: false,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            position: "absolute",
-          },
-          web: {
-            position: "fixed",
-            bottom: 0,
-            left: "50%",
-            transform: "translateX(-50%)",
-            width: "100%",
-            maxWidth: 480,
-
-            backgroundColor: theme.colors.background,
-          },
-          default: {},
-        }),
-      }}
-    >
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
