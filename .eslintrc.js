bbac697d3515d04a3d173908f5a8a939154d8e1c
adriandp.ext <adriandp.ext@gmail.com>
2025-02-10 15:46:42 +0800

    lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: "expo",
+  extends: ["expo", "plugin:@typescript-eslint/recommended"],
   ignorePatterns: ["/dist/*"],
+  rules: {
+    "no-console": "error",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/jsx-filename-extension": [
+      "warn",
+      { extensions: [".jsx", ".js", ".tsx", ".ts"] },
+    ],
+  },
 };
