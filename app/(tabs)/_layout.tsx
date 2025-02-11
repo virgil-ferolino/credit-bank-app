4507b1a53557e5906cfb3fbecbcffa7109d39b87
adriandp.ext <adriandp.ext@gmail.com>
2025-02-11 13:09:18 +0800

    layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,30 +2,52 @@ import { Tabs } from "expo-router";
 import React from "react";
 
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import {
+  OpaqueColorValue,
+  StyleProp,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
+import { SFSymbol } from "expo-symbols";
+
+const tabBarStyle: StyleProp<ViewStyle> = {
+  maxWidth: 480,
+  width: "100%",
+  height: 50,
+  alignSelf: "center",
+};
+
+const tabBarLabelStyle: StyleProp<TextStyle> = {
+  fontSize: 10,
+  fontWeight: 900,
+};
+
+const TabBarIcon = ({
+  color,
+  name,
+}: {
+  color: string | OpaqueColorValue;
+  name: SFSymbol;
+}) => {
+  return <IconSymbol size={28} name={name} color={color} />;
+};
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarStyle: {
-          maxWidth: 480,
-          width: "100%",
-          height: 50,
-          alignSelf: "center",
-        },
+        tabBarStyle: tabBarStyle,
         tabBarLabelPosition: "below-icon",
-        tabBarLabelStyle: {
-          fontSize: 10,
-          fontWeight: 900,
-        },
+        tabBarLabelStyle: tabBarLabelStyle,
+        tabBarAllowFontScaling: true,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: (props) => (
+            <TabBarIcon color={props.color} name="house.fill" />
           ),
           headerShown: false,
         }}
@@ -34,8 +56,8 @@ export default function TabLayout() {
         name="mycard"
         options={{
           title: "My Card",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="creditcard.fill" color={color} />
+          tabBarIcon: (props) => (
+            <TabBarIcon color={props.color} name="creditcard.fill" />
           ),
           headerShown: false,
         }}
@@ -44,8 +66,8 @@ export default function TabLayout() {
         name="settings"
         options={{
           title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="gear.circle.fill" color={color} />
+          tabBarIcon: (props) => (
+            <TabBarIcon color={props.color} name="gear.circle.fill" />
           ),
           headerShown: false,
         }}
