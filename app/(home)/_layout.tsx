036096ed9f428f9f0d51d9535feaf91de9482b11
adriandp.ext <adriandp.ext@gmail.com>
2025-02-18 16:22:17 +0800

    remove comment

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,55 +1,10 @@
 import React from "react";
 
 import { Stack } from "expo-router";
-// import { Ionicons } from "@expo/vector-icons";
-// import { useAppTheme } from "@/hooks/useTheme";
-// import { Platform, TouchableOpacity } from "react-native";
+
 import StackLayout from "@/components/StackLayout";
 const HomeLayout = () => {
-  // const router = useRouter();
-  // const theme = useAppTheme();
   return (
-    // <Stack
-    //   screenOptions={{
-    //     animation: "slide_from_left",
-    //     headerLeft: () => {
-    //       return Platform.select({
-    //         web: (
-    //           <TouchableOpacity onPress={router.back}>
-    //             <Ionicons
-    //               name="arrow-undo-circle-outline"
-    //               size={30}
-    //               color={theme.colors.primary}
-    //               style={{ paddingLeft: 8 }}
-    //             />
-    //           </TouchableOpacity>
-    //         ),
-    //         android: (
-    //           <TouchableOpacity onPress={router.back}>
-    //             <Ionicons
-    //               name="arrow-undo-circle-outline"
-    //               size={30}
-    //               color={theme.colors.primary}
-    //             />
-    //           </TouchableOpacity>
-    //         ),
-    //         ios: (
-    //           <TouchableOpacity onPress={router.back}>
-    //             <Ionicons
-    //               name="arrow-undo-circle-outline"
-    //               size={30}
-    //               color={theme.colors.primary}
-    //             />
-    //           </TouchableOpacity>
-    //         ),
-    //       });
-    //     },
-    //     headerTitleStyle: {
-    //       fontFamily: "Poppins",
-    //     },
-    //   }}
-    // >
-
     <StackLayout>
       <Stack.Screen
         name="notification/index"
