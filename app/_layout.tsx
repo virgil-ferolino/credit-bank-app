daeab729fbb4c5ccaa409b218845e1df0f76d6dd
sushikansai <michaelsagun013@gmail.com>
2025-02-21 17:59:42 +0800

    custom splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "react-native-reanimated";
 import { PaperProvider } from "react-native-paper";
 import theme from "@/theme";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import { View, ImageBackground } from "react-native";
+import { Image } from "expo-image";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,6 +20,8 @@ const ContainedView = styled(View)({
 });
 
 export default function RootLayout() {
+  const [isSplashVisible, setIsSplashVisible] = useState<boolean>(true);
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     Poppins: require("../assets/fonts/Poppins-Regular.ttf"),
@@ -31,12 +34,40 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (loaded) {
+      setTimeout(() => {
+        setIsSplashVisible(false);
+      }, 4000);
       SplashScreen.hideAsync();
     }
   }, [loaded]);
 
   if (!loaded) {
     return null;
+  }
+
+  if (isSplashVisible) {
+    return (
+      <ImageBackground
+        source={require("@/assets/images/splash-background.png")}
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          position: "absolute",
+          left: "50%",
+          transform: [{ translateX: -240 }],
+          width: 480,
+        }}
+      >
+        <Image
+          source={require("@/assets/images/alphabank.gif")}
+          style={{
+            width: 400,
+            height: "50%"
+          }}
+        />
+      </ImageBackground>
+    )
   }
 
   return (
